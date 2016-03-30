13c74a028476c228b7328babab305561845f4df3
gvanderclay <2fat4doors>
2016-03-30 13:35:00 -0400

    Add some classes that will be in our program

---
+++ src/ai/gage/mitch/checkers/ai/gage/mitch/checkers/model/GameBoard.java
@@ -0,0 +1,12 @@
+package ai.gage.mitch.checkers.ai.gage.mitch.checkers.model;
+
+/**
+ * Holds data that represents the gameboard
+ */
+public class GameBoard {
+
+    /* 2d array that represents the board */
+    Piece[][] board;
+
+
+}
