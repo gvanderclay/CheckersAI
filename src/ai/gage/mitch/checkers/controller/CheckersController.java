4f4251f2336095ffda6a9083f7cd9c7bfd4b03bd
gvanderclay <2fat4doors>
2016-03-30 14:37:05 -0400

    Move classes around so it is more organized

--- src/ai/gage/mitch/checkers/controller/CheckersController.java
+++ src/ai/gage/mitch/checkers/controller/CheckersController.java
@@ -1,9 +1,13 @@
 package ai.gage.mitch.checkers.controller;
 
+import ai.gage.mitch.checkers.model.GameBoard;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 
 public class CheckersController {
+
+    /* Holds the logic for the game board */
+    private GameBoard gameBoard;
 
     @FXML
     private Pane boardPane;
