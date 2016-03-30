62fc63264542832e94eb32e2d0e3c93c1dff732f
gvanderclay <2fat4doors>
2016-03-30 15:35:29 -0400

    Add logic for player class

--- src/ai/gage/mitch/checkers/model/Player.java
+++ src/ai/gage/mitch/checkers/model/Player.java
@@ -3,5 +3,16 @@ package ai.gage.mitch.checkers.model;
 /**
  * Created by vandercg on 3/30/16.
  */
-public class Player {
+public enum Player {
+
+    BLACK, WHITE;
+
+    /**
+     * Return the {@code Player} whose turn is next.
+     *
+     * @return the {@code Player} whose turn is next
+     */
+    public Player next() {
+        return this == BLACK ? WHITE : BLACK;
+    }
 }
