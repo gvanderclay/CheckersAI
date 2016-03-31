165fb6c69c52dc093101d622c2802841c589dba5
couturmi <couturmi@mail.gvsu.edu>
2016-03-31 09:51:22 -0400

    isValidMove edits

--- src/ai/gage/mitch/checkers/model/Piece.java
+++ src/ai/gage/mitch/checkers/model/Piece.java
@@ -8,6 +8,9 @@ public class Piece {
 
     /* Player that owns this piece */
     private Player owner;
+	
+	/* Is true if the piece has become a King */
+	private boolean isKing;
 
 
     /****************************************************************
@@ -23,11 +26,28 @@ public class Piece {
         return this.owner;
     }
 
-    /**
-     * Whether or not the move is valid
-     ***/
-    public boolean isValidMove() {
-
+    /*************************************************************************
+     * Determines whether or not the move is valid
+	 * @param fromRow 		row that the piece is starting from
+	 * @param fromColumn	column that the piece is starting from
+	 * @param toRow			row that the piece is moving to
+	 * @param toColumn		column that the piece is moving to
+     *************************************************************************/
+    public boolean isValidMove(int fromRow, int fromColumn, int toRow, int toColumn) {
+		//If this piece is not a King
+		if(!isKing){
+			if(owner == Player.WHITE){
+				//check moves for one direction (SOUTH)
+			}
+			else if(owner == Player.BLACK){
+				//check moves for other direction (NORTH)
+			}
+		}
+		//If this piece is a King
+		else{
+			//check moves for any direction
+		}
+	
         return false;
     }
 }
